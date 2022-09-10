fa1fcd7b7884c69e5f720145b7df02e1264a3af3
curly-talez <krishnaveni.skps@gmail.com>
2022-09-10 19:39:45 +0530

    added basic changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,60 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'scoreBoard';
+  frames = [1,2,3,4,5,6,7,8,9,10];
+
+  isStrike = false;
+  isSpare = false;
+
+  currentScore = 0;
+  remainingRollesForFrame = 2; 
+  currentFrameIndex = 0;
+  currentFrame = 0;
+
+  newBall() {
+    this.currentFrame = this.frames[this.currentFrameIndex];
+    console.log('Current Frame: ', this.currentFrame);
+
+    if (this.remainingRollesForFrame === 2) {
+      this.firstRoll();
+    } else {
+      this.secondRoll();
+    }
+  }
+
+  firstRoll() {
+    this.currentScore = this.generateRandomNumber(11);
+    if(this.currentScore === 10) { // strike
+      this.remainingRollesForFrame = 2;
+      this.currentFrameIndex++;
+    } else { // not a strike
+      this.remainingRollesForFrame--;
+    }
+    console.log('This is first roll!--------', this.currentScore);
+  }
+
+  secondRoll(secRoll?: number) {
+    this.remainingRollesForFrame = 2;
+    this.currentFrameIndex ++;
+    this.currentScore = this.generateRandomNumber(11-this.currentScore);
+    console.log('This is second roll!---------', this.currentScore);
+  }
+
+  generateRandomNumber (roll: number) {
+    return  Math.floor(Math.random() * roll)
+  }
 }
+
+/***
+  1. Bowl
+    cases
+    1. first bawl
+      - 10 points
+      - < 10 points
+      - 0 points
+    2. Second bawl
+      - 10 points
+      - first and second 10 points =>  spare
+      - first and second  < 10 points
+      - second ball 0 points
+ */
