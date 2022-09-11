78d7a45d7d45c2d83d5b06c30de762a43ef12f8c
curly-talez <krishnaveni.skps@gmail.com>
2022-09-11 11:29:26 +0530

    added map for frames to track frame details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'scoreBoard';
   frames = [1,2,3,4,5,6,7,8,9,10];
 
@@ -17,33 +17,59 @@ export class AppComponent {
   currentFrameIndex = 0;
   currentFrame = 0;
 
+  frameDetailsMap: any = {};
+  framesWithAdditionalRolls: number[] = []
+  
+  ngOnInit(): void {
+    for (let i = 1;  i < this.frames.length+1; i++) {
+      this.frameDetailsMap[i] = {
+        frame: i,
+        scores: [],
+        totalScores: 0,
+        additionalRolls: 0,
+        isSpare: false,
+        isStrike: false
+      }
+    }
+  }
+
   newBall() {
     this.currentFrame = this.frames[this.currentFrameIndex];
-    console.log('Current Frame: ', this.currentFrame);
-
     if (this.remainingRollesForFrame === 2) {
       this.firstRoll();
     } else {
       this.secondRoll();
     }
+    console.log('New Ball ------------------', this.frameDetailsMap[this.currentFrame]),
+    console.log('framesWithAdditionalRolls--------------', this.framesWithAdditionalRolls)
   }
 
   firstRoll() {
     this.currentScore = this.generateRandomNumber(11);
     if(this.currentScore === 10) { // strike
+      this.frameDetailsMap[this.currentFrame].isStrike = true;
       this.remainingRollesForFrame = 2;
       this.currentFrameIndex++;
+      this.frameDetailsMap[this.currentFrame].additionalRolls = 2;
+      this.framesWithAdditionalRolls.push(this.currentFrame);
     } else { // not a strike
       this.remainingRollesForFrame--;
     }
-    console.log('This is first roll!--------', this.currentScore);
+    this.frameDetailsMap[this.currentFrame].scores.push(this.currentScore);
+    this.frameDetailsMap[this.currentFrame].totalScores += this.currentScore;
   }
 
-  secondRoll(secRoll?: number) {
+  secondRoll() {
     this.remainingRollesForFrame = 2;
     this.currentFrameIndex ++;
     this.currentScore = this.generateRandomNumber(11-this.currentScore);
-    console.log('This is second roll!---------', this.currentScore);
+    this.frameDetailsMap[this.currentFrame].scores.push(this.currentScore);
+    this.frameDetailsMap[this.currentFrame].totalScores += this.currentScore;
+    if (this.frameDetailsMap[this.currentFrame].totalScores === 10) { // spare
+      this.frameDetailsMap[this.currentFrame].isSpare = true;
+      this.frameDetailsMap[this.currentFrame].additionalRolls = 1;
+      this.framesWithAdditionalRolls.push(this.currentFrame);
+    }
   }
 
   generateRandomNumber (roll: number) {
